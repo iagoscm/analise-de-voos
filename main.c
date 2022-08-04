36dc50c392229ec5dfc8f3ce1f9be58005c171c2
iagoscm <iagomatosousa@gmail.com>
2022-08-04 11:19:05 -0300

    retira coluna id do arquivo

--- main.c
+++ main.c
@@ -1,13 +1,23 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main(){
 
-    char primeiraLinha[50];
-    FILE *arqOriginal;
+    char firstLine[50];
+    int delay;
+    char airline[2];
+    FILE *originalFile;
 
-    arqOriginal = fopen("/files/Airlines.csv", "r");
-    fscanf(arqOriginal, "%s", primeiraLinha);
-    printf("%s", primeiraLinha);
+    originalFile = fopen("files/Airlines.csv", "r");
+    fscanf(originalFile, "%s", firstLine);
+    printf("%s", firstLine);
+
+    if(originalFile){
+        while(fscanf(originalFile, "%[^,],%d", airline, &delay) != EOF){
+            //
+        }
+        //
+    }
 
     return 0;
 }
