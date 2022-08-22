3ca8665b81ccef7e98c6cd8ca4d5009b3617b2e9
Natan Tavares Santana <CIANDT\natanns@lnb035255cps.ciandt.global>
2022-08-22 20:21:06 -0300

    Bubble sort

--- main.c
+++ main.c
@@ -7,20 +7,13 @@ typedef struct {
     short int delay;
 } rawData;
 
-int contadorLinhas() {
-    FILE *fp;
-    char linha[50];
-    int count = 0;
+int contadorLinhas();
 
-    fp = fopen("./files/Airlines.csv", "r");
-    fscanf(fp,"%s",linha);
-    while (fscanf(fp,"%s",linha) != EOF)
-    {
-        count++;
-    }
-    return count;
-    
-}
+int comparaStr(char str1[], char str2[]);
+
+int copiaStr(char str1[], char str2[]);
+
+int bubbleSortAlph(rawData data1[],int tamStr);
 
 int main(){
     clock_t t;
@@ -43,6 +36,11 @@ int main(){
     {
         indice ++;
     }
+
+    bubbleSortAlph(dadosOriginais, quantidadeLinhas);
+    
+
+
     
 
     t = clock() - t;
@@ -54,4 +52,68 @@ int main(){
     
 
     return 0;
+}
+
+int contadorLinhas() {
+    FILE *fp;
+    char linha[50];
+    int count = 0;
+
+    fp = fopen("./files/Airlines.csv", "r");
+    fscanf(fp,"%s",linha);
+    while (fscanf(fp,"%s",linha) != EOF)
+    {
+        count++;
+    }
+    return count;
+    
+}
+
+int comparaStr(char str1[], char str2[]) {
+    int i = 0;
+
+    while (str1[i] == str2[i] && str1[i] != '\0')
+    {        
+        i++;
+    }
+
+    if (str1[i] < str2[i])
+    {
+        return -1;
+    } else if (str1[i] > str2[i])
+    {
+        return 1;
+    } else {
+        return 0;
+    } 
+    
+}
+
+int copiaStr(char str1[], char str2[]) {
+    for (int i = 0; i < 3; i++)
+    {
+        str1[i] = str2[i];
+    }
+    
+}
+
+int bubbleSortAlph(rawData data1[],int tamStr) {
+    int i, j;
+    rawData tempData;
+
+    for (i = tamStr - 1; i > 0; i--)
+    {
+        for (j = 0; j < i; j++)
+        {
+            if (comparaStr(data1[j].nome, data1[j+1].nome) > 0)
+            {
+                tempData = data1[j];
+                data1[j] = data1[j+1];
+                data1[j+1] = tempData;
+            }
+            
+        }
+        
+    }
+    
 }
