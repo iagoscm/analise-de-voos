980759b3306388c0c3973bebc40561959ca62c8a
iagoscm <iagomatosousa@gmail.com>
2022-08-24 21:40:59 -0300

    função que gera gráfico em py

---
+++ geraGrafico.py
@@ -0,0 +1,24 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+
+df = pd.read_csv('files/Airlines_grafico.csv')
+fig = plt.figure()
+ax = fig.add_axes([0,0,1,1])
+
+# Atribuindo os valores do dataframe para as variáveis
+airlines = df['Airlines']
+delays = df[' Media de atrasos']
+
+plt.figure(figsize=(10,6))
+
+# Rótulo do eixo x
+plt.xlabel("Media de atrasos")
+
+# Rótulo do eixo y
+plt.ylabel("Linhas aéreas")
+
+# Adicionando as variáveis para o gráfico
+plt.plot(airlines, delays, 'r')
+
+# Mostrar o gráfico
+plt.show()
